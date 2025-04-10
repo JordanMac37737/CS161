08cb2934a648a4fd6536d1c2df5ad67db8e94535
JordanMac37737 <jordan.maccormack@student.cgcc.edu>
2025-04-10 10:42:47 -0700

    Add files via upload

---
+++ calculate.java
@@ -0,0 +1,16 @@
+package Main;
+
+public class calculate {
+
+static double a=20;
+static double b=1.5;
+
+
+public static void main(String[] args) {
+calculate myObject = new calculate();
+myObject.calculate();
+}
+public void calculate(){
+	System.out.println(a*b);
+}
+}
