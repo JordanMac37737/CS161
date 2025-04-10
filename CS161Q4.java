d90ccf8c81ab2800a357d079706d716a36c2d38c
JordanMac37737 <jordan.maccormack@student.cgcc.edu>
2025-04-10 09:42:28 -0700

    Add files via upload

---
+++ CS161Q4.java
@@ -0,0 +1,17 @@
+package Main;
+
+public class CS161Q4 {
+
+	String helloWorld;
+	
+public static void main(String[] args) {
+	CS161Q4 myObject = new CS161Q4();
+	myObject.MyString();
+}
+CS161Q4(){
+	helloWorld=("Hello World!");
+}
+public void MyString(){
+System.out.print(helloWorld);
+}
+}
