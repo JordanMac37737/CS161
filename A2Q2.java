6908c0f5d4dee1c5b5b1dfff4b8c0c2fdded62ed
JordanMac37737 <jordan.maccormack@student.cgcc.edu>
2025-04-15 10:51:15 -0700

    Add files via upload

---
+++ A2Q2.java
@@ -0,0 +1,13 @@
+package CS161;
+
+public class A2Q2 {
+
+static boolean a =true;
+static boolean b =true;
+static boolean result =a||b;
+
+public static void main(String[] args) {
+System.out.println(result);
+}
+}
+
