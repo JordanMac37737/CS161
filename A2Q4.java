bfa1310242f91234037eebbbcf3cc1903047e679
JordanMac37737 <jordan.maccormack@student.cgcc.edu>
2025-04-17 09:56:42 -0700

    Add files via upload

---
+++ A2Q4.java
@@ -0,0 +1,22 @@
+package CS161;
+
+import java.util.Scanner;
+
+public class A2Q4 {
+
+	public static void main(String[] args) {
+	Scanner in = new Scanner (System.in);
+	
+	System.out.println("test score: ");
+	double i = in.nextDouble();
+	
+	if (i < 50)
+	{System.out.println("fail");}
+	
+	 
+	else
+	{
+		System.out.println("pass");
+	}
+	}
+}
